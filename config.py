d3fd3f3f86b78e500ff85854d2038cc87bec6323
steeve02597 <119192572+steeve02597@users.noreply.github.com>
2025-06-30 15:12:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,55 +1,53 @@
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import os
-from os import environ,getenv
+from os import environ, getenv
 import logging
 from logging.handlers import RotatingFileHandler
 
-#rohit_1888 on Tg
-#--------------------------------------------
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
-#--------------------------------------------
+# Credits (Do Not Remove)
+# @CodeFlix_Bots | @rohit_1888
+# Telegram Support: @CodeflixSupport
+# GitHub: https://github.com/Codeflix-Bots/FileStore
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "sewxiy") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "776376926")) # Owner id
-#--------------------------------------------
-PORT = os.environ.get("PORT", "8001")
-#--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
+# =============== ENV VARIABLES ===============
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339")  # Old bot token (used as default)
+BOT_TOKEN = TG_BOT_TOKEN  # new style alias
+
+APP_ID = int(os.environ.get("APP_ID", ""))  # From my.telegram.org
+API_ID = APP_ID  # alias for new format
+
+API_HASH = os.environ.get("API_HASH", "")  # From my.telegram.org
+DB_URI = os.environ.get("DATABASE_URL", "")  # alias for new format
+DATABASE_URL = DB_URI  # backwards compatible alias
+
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
-#--------------------------------------------
-FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/S_B_G01")
+
+# New variables (will add in Koyeb)
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "0"))
+ADMIN_IDS = os.environ.get("ADMIN_IDS", "")  # Comma-separated string or single ID
+
+# =============== OTHER CONFIGS ===============
+
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388"))
+OWNER = os.environ.get("OWNER", "sewxiy")
+OWNER_ID = int(os.environ.get("OWNER_ID", "776376926"))
+
+PORT = os.environ.get("PORT", "8001")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
-#--------------------------------------------
+
+FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/S_B_G01")
+
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/39d6cde9bc4d7cfc0d266.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/39d6cde9bc4d7cfc0d266.jpg")
-#--------------------------------------------
 
-#--------------------------------------------
-HELP_TXT = "<b>This is a file sharing bot created for cinema company</b>"
-ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/S_B_G01>Steeve</a>\n◈ Series ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/seriescc>Cinema Company Series</a></blockquote></b>"
-#--------------------------------------------
-#--------------------------------------------
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n<blockquote> you can use me for accessing series, Join <a href=https://t.me/seriescc>Cinema Company Series</a> for series </blockquote></b>")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
 
-CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
+HELP_TXT = "<b>This is a file sharing bot created for cinema company</b>"
+ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/S_B_G01>Steeve</a>\n◈ Series ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/seriescc>Cinema Company Series</a></blockquote></b>"
 
+CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
 <b>›› /dlt_time :</b> sᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
 <b>›› /check_dlt_time :</b> ᴄʜᴇᴄᴋ ᴄᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
 <b>›› /dbroadcast :</b> ʙʀᴏᴀᴅᴄᴀsᴛ ᴅᴏᴄᴜᴍᴇɴᴛ / ᴠɪᴅᴇᴏ
@@ -65,17 +63,15 @@ CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockqu
 <b>›› /deladmin :</b> ʀᴇᴍᴏᴠᴇ ᴀɴ ᴀᴅᴍɪɴ
 <b>›› /admins :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ᴀᴅᴍɪɴs
 """
-#--------------------------------------------
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>") #set your Custom Caption here, Keep None for Disable Custom Caption
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
-#--------------------------------------------
-#Set true if you want Disable your Channel Posts Share button
+
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>")
+PROTECT_CONTENT = True if os.environ.get("PROTECT_CONTENT", "False") == "True" else False
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-#--------------------------------------------
+
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
-#--------------------------------------------
 
+# =============== LOGGING ===============
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -86,7 +82,7 @@ logging.basicConfig(
     handlers=[
         RotatingFileHandler(
             LOG_FILE_NAME,
-            maxBytes=50000000,
+            maxBytes=50_000_000,
             backupCount=10
         ),
         logging.StreamHandler()
@@ -94,7 +90,5 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   

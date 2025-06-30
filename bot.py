a1f51eb52158ff88caec87e80bfa3a9055713786
steeve02597 <119192572+steeve02597@users.noreply.github.com>
2025-06-30 14:52:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,16 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-#rohit_1888 on Tg
 from config import *
 
-
-name ="""
- BY CODEFLIX BOTS
-"""
+from handlers.start import register_start
+from handlers.gfilter import register_gfilter
+from handlers.user_filters import register_user_filter
+from handlers.alert_handler import register_alert_handler
+from handlers.viewfilters import register_viewfilters
+from handlers.delete_filter import register_delete_filter
+from handlers.request_handler import register_request_handler
+from handlers.request_callback import register_request_callback
 
 
 class Bot(Client):
@@ -21,9 +24,7 @@ class Bot(Client):
             name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={
-                "root": "plugins"
-            },
+            plugins={"root": "plugins"},
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
         )
@@ -37,52 +38,47 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeflixSupport for support")
+            self.LOGGER(__name__).warning(f"Make sure bot is admin in DB Channel, and check CHANNEL_ID ({CHANNEL_ID})")
+            self.LOGGER(__name__).info("Bot stopped. Join @CodeflixSupport for support.")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
-
-        self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot started as @{usr_bot_me.username}")
 
-        # Start Web Server
+        register_gfilter(self)
+        register_start(self)
+        register_viewfilters(self)
+        register_delete_filter(self)
+        register_request_handler(self)
+        register_request_callback(self)
+        register_alert_handler(self)
+        register_user_filter(self)
+
         app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
-
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
-        except: pass
+        try:
+            await self.send_message(OWNER_ID, text="<b><blockquote>Bot Restarted by @Codeflix_Bots</blockquote></b>")
+        except:
+            pass
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
 
     def run(self):
-        """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is running.")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
             self.LOGGER(__name__).info("Shutting down...")
         finally:
             loop.run_until_complete(self.stop())
-
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
